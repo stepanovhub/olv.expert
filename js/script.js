57a2e1189cb81768f9cf5065424b031f5d4f91be
stepanov <step@m-games-ltd.com>
2018-09-13 12:41:58 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  var typed = new Typed('#typed', {
+    strings: [
+        "shows how agencies and platforms ^500\nspend your money.",
+        "gives you arguments ^500\nto reduce OLV buying price.",
+        "is simple.",
+        "doesn't take servers to be used.",
+        "will take care about data by it's own staff.",
+        "can save about 10% of your OLV media budget."
+    ],
+    typeSpeed: 100,
+    backSpeed: 10,
+    cursorChar: '_',
+    backDelay: 2000,
+    startDelay: 500,
+    fadeOut: true,
+    loop: true
+  });
+})
